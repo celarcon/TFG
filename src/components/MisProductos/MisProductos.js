b3b8e1473e183d9416d7c65bd52efdec643ba0c6
unknown <santicelarcon@gmail.com>
2020-07-11 18:39:12 +0200

    11/07/2020

--- src/components/MisProductos/MisProductos.js
+++ src/components/MisProductos/MisProductos.js
@@ -1,32 +1,44 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
+import jwt_decode from 'jwt-decode';
 
 class MisProductos extends Component {
 
     state = {
-        productos:[]
+        productos:[],
+        idPropietario: ' '
     }
 
     async componentDidMount(){
-        const res = await Axios.get('http://localhost:4000/products');
+
+        //sacamos el id del propietario
+        const token = localStorage.usertoken;
+        const decoded = jwt_decode(token);
+        this.setState({
+          idPropietario: decoded._id
+        });
+        console.log(this.state.idPropietario);
+        const res = await Axios.get('http://localhost:4000/products/misProductos/' + decoded._id);
         this.setState({productos: res.data});
 
-        console.log(this.state.productos);
+        //console.log(this.state.idPropietario);
     }
 
     render() {
         return (
-            <div className="row">
+            <div className="col-md-8">
                 <h1>MisProductos</h1>
+                <ul className="list-group">
                 {
                     this.state.productos.map(productos =>
-                    <li>
+                    <li className="list-group-item">
                         {productos.nombre}
                         <img src={'http://localhost:4000/products/get-image/'+productos.image} alt={productos.nombre} width="100"></img>
                         {productos.descripcion}
                     </li>
                     )
                 }
+                </ul>
             </div>
         )}
 
